c56ed0924745171ef7d5a6ed15b53c9de5b57aad
ShimmerGlass <mathildegilles49@gmail.con>
2025-11-20 12:21:24 +0100

    fix(metrics): name field

--- lib/prov/metrics.go
+++ lib/prov/metrics.go
@@ -7,37 +7,37 @@ import (
 
 var (
 	metricSourceStatus = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_source_status",
-		Help:      "1=Success 0=Failed",
+		Name: "shimdns_source_status",
+		Help: "1=Success 0=Failed",
 	}, []string{"name"})
 
 	metricSourceRecords = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_source_records",
-		Help:      "Number of records fetched by source",
+		Name: "shimdns_source_records",
+		Help: "Number of records fetched by source",
 	}, []string{"name"})
 
 	metricSourceFetchTime = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_source_fetch_time_seconds",
-		Help:      "Time taken fetching the source",
+		Name: "shimdns_source_fetch_time_seconds",
+		Help: "Time taken fetching the source",
 	}, []string{"name"})
 
 	metricModifierStatus = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_modifier_status",
-		Help:      "1=Success 0=Failed",
+		Name: "shimdns_modifier_status",
+		Help: "1=Success 0=Failed",
 	}, []string{"name"})
 
 	metricModifierApplyTime = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_modifier_apply_time_seconds",
-		Help:      "Time taken applying the modifier",
+		Name: "shimdns_modifier_apply_time_seconds",
+		Help: "Time taken applying the modifier",
 	}, []string{"name"})
 
 	metricSinkStatus = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_sink_status",
-		Help:      "1=Success 0=Failed",
+		Name: "shimdns_sink_status",
+		Help: "1=Success 0=Failed",
 	}, []string{"name"})
 
 	metricSinkWriteTime = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "shimdns_sink_write_time_seconds",
-		Help:      "Time taken writing to the sink",
+		Name: "shimdns_sink_write_time_seconds",
+		Help: "Time taken writing to the sink",
 	}, []string{"name"})
 )
